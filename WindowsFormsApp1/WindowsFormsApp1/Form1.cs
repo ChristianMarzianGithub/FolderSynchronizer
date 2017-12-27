1609e26c1a42ca1046bafc3fac506733fdb13d25
randwerArbeit <marzian@innosoft.de>
2017-12-27 13:55:15 +0100

    Projektstruktur und GUI angelegt

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void folderBrowserDialog1_HelpRequest(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.ShowDialog();
+            if(folderBrowserDialog1.SelectedPath != "")
+            {
+                button2.Enabled = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {            
+            folderBrowserDialog2.ShowDialog();
+            if ((folderBrowserDialog1.SelectedPath != "") && (folderBrowserDialog2.SelectedPath != ""))
+            {
+                button3.Enabled = true;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            ProgrammCalc pc = new ProgrammCalc(folderBrowserDialog1.SelectedPath, folderBrowserDialog2.SelectedPath);
+            if ((folderBrowserDialog1.SelectedPath != "") && (folderBrowserDialog2.SelectedPath != "") && (folderBrowserDialog2.SelectedPath != folderBrowserDialog1.SelectedPath))
+            {
+                pc.synchronize();
+            }
+        }
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/ProgrammCalc.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    class ProgrammCalc
+    {
+        string source;
+        string destination;
+        public ProgrammCalc(string source, string destination)
+        {
+            this.source = source;
+            this.destination = destination;
+        }
+
+        public void synchronize()
+        {
+
+        }
+    }
+}
